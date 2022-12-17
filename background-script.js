025432b6856863a7504bfd41411cc4e751c34c07
Abigail Ponce <abigaiil221b@gmail.com>
2022-12-16 18:03:30 -0800

    Remove file

--- background-script.js
+++
@@ -1,7 +0,0 @@
-
-/**
- * Listen for message from sidebar then send it to the content script
- */
-browser.runtime.onMessage.addListener((message) => {
-    browser.tabs.sendMessage(message.tabId, { time: message.time });
-});
